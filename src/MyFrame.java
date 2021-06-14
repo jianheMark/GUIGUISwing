9d66fad6ac80feaa8fa4b565b1f6d1400ce5b602
jianheMark <jianhe.mark@pm.me>
2021-06-14 10:03:25 +0530

    frame size

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -6,7 +6,7 @@ public class MyFrame extends JFrame {
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application.
         //https://docs.oracle.com/javase/7/docs/api/javax/swing/JFrame.html#setDefaultCloseOperation%28int%29
-        this.setSize(420, 420); //set the size.
+        this.setSize(750, 750); //set the size.
         this.setResizable(true);//resize the frame.
         this.setTitle("Welocme to new world."); //set the Title.
         ImageIcon imageIcon = new ImageIcon("logo.png");
